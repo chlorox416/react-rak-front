415b7f94925083fb9b165f2873bf4c344aaf47d7
chlorox416 <chloechang416@gmail.com>
2020-12-15 23:28:52 -0500

    save w/ state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import Homepage from './components/Homepage';
+import CategoryContainer from './components/CategoryContainer';
+// import CategoryContainer from './components/Favorites'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  state={
+    kindnessArray: [],
+    }
+
+
+componentDidMount() {
+  fetch('http://localhost:3000/api/v1/kindnesses')
+  .then(resp => resp.json())
+  .then(data => console.log(data))
 }
+
+
+  render() {
+      return (
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <Route exact path="/" component={Homepage}/>
+              <Route exact path="/allcategories" component={CategoryContainer}/>
+            </header>
+          </div>
+        </Router>
+      )
+}
+
+
+}
+
 
 export default App;
---
+++ src/components/CategoryContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CategoryContainer extends Component {
+    render() {
+        return (
+            <div>
+                category container!
+            </div>
+        )
+    }
+}
